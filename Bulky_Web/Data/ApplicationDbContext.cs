bbe5795dc132915e5d330c83ef5d956fb4053aad
Agiratech-Kiruthika <kiruthika.p@agiratech.com>
2024-02-13 16:41:52 +0530

    Add project files.

---
+++ Bulky_Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Bulky_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bulky_Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        //ctor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1,Name="Action",DisplayOrder= "1"},
+                new Category { Id=2, Name="Scifi", DisplayOrder= "2" },
+                new Category { Id=3, Name="History", DisplayOrder= "3" }
+
+
+
+                );
+
+        }
+
+    }
+}
